a44f684d7c7706f4733d5c160e52cd4f44b4ec82
user_zf <519076284@qq.com>
2017-05-15 22:55:02 +0800

    1、提交新创建的项目，后续会往项目里面添加内容

---
+++ app/src/main/java/user_zf/self/rulelife/MainActivity.java
@@ -0,0 +1,13 @@
+package user_zf.self.rulelife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
